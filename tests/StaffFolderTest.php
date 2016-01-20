e9138218ef3a56d463b2f2eb4bf31b6eb0b58593
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 15:17:36 +0700

    MINOR: Added Staff cms fields test.  Converted to PSR2

--- tests/StaffFolderTest.php
+++ tests/StaffFolderTest.php
@@ -1,24 +1,29 @@
 <?php
 
-class StaffFolderTest extends SapphireTest {
-	public function testGetCMSFields() {
-		$this->markTestSkipped('TODO');
-	}
+class StaffFolderTest extends SapphireTest
+{
+    public function testGetCMSFields()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testGetPortletTitle() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testGetPortletTitle()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testGetPortletImage() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testGetPortletImage()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testGetPortletCaption() {
-		$this->markTestSkipped('TODO');
-	}
+    public function testGetPortletCaption()
+    {
+        $this->markTestSkipped('TODO');
+    }
 
-	public function testStaffFolderCacheKey() {
-		$this->markTestSkipped('TODO');
-	}
-
+    public function testStaffFolderCacheKey()
+    {
+        $this->markTestSkipped('TODO');
+    }
 }
--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -1,8 +1,29 @@
 <?php
 
-class StaffTest extends SapphireTest {
-	public function testGetCMSFields() {
-		$this->markTestSkipped('TODO');
-	}
+class StaffTest extends SapphireTest
+{
+    public function testGetCMSFields()
+    {
+        $staff = new Staff();
+        $fields = $staff->getCMSFields();
+        $imageFields = $this->getFieldNamesForTab($fields, 'Root.Image');
+        $this->assertEquals(array('Photo'), $imageFields);
+
+        $jobFields = $this->getFieldNamesForTab($fields, 'Root.JobDetail');
+        $expected = array('JobTitle', 'Email', 'TelephoneNumberDesk',
+                                'TelephoneNumberMobile');
+        $this->assertEquals($expected, $jobFields);
+    }
+
+
+    private function getFieldnamesForTab($fields, $tabName) {
+        $tab = $fields->findOrMakeTab($tabName);
+        $fields = $tab->fieldList();
+        $names = array();
+        foreach ($fields as $field) {
+            array_push($names, $field->getName());
+        }
+        return $names;
+    }
 
 }
