a58efc1b6e5a53308131d920450b21362be4353f
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 15:17:02 +0700

    MINOR: PSR2 and reformat

--- code/Staff.php
+++ code/Staff.php
@@ -1,50 +1,42 @@
 <?php
 /**
-* Defines the StaffPage page type
+* Defines the StaffPage page type.
 */
-class Staff extends Page {
-  static $db = array(
-  'TelephoneNumberDesk' => 'Varchar',
-  'TelephoneNumberMobile' => 'Varchar',
-  'Email' => 'Varchar',
-  'ShowOnHomePage' => 'Boolean',
-  'JobTitle' => 'Text'
-  );
-  static $has_one = array(
-  'Photo' => 'Image'
-  );
+class Staff extends Page
+{
+    public static $db = array(
+        'TelephoneNumberDesk' => 'Varchar',
+        'TelephoneNumberMobile' => 'Varchar',
+        'Email' => 'Varchar',
+        'ShowOnHomePage' => 'Boolean',
+        'JobTitle' => 'Text',
+    );
 
-  private static $icon = 'weboftalent-staff/icons/employee.png'; 
+    public static $has_one = array(
+        'Photo' => 'Image',
+    );
 
-  
-  
-  function getCMSFields() {
-    $fields = parent::getCMSFields();
-    $fields->addFieldToTab("Root.Image", new UploadField('Photo'));
+    private static $icon = 'weboftalent-staff/icons/employee.png';
 
-    $fields->addFieldToTab("Root.JobDetail", new TextField('JobTitle', _t('Staff.JOB_TITLE', 'Job Title')));
-    
-    $fields->addFieldToTab("Root.JobDetail", new EmailField('Email'));
-    $fields->addFieldToTab("Root.JobDetail", new TextField('TelephoneNumberDesk'));
-    $fields->addFieldToTab("Root.JobDetail", new TextField('TelephoneNumberMobile'));
-    
-    $fields->renameField("TelephoneNumberDesk", "Telephone Number (Landline)");
-    $fields->renameField("TelephoneNumberMobile", "Telephone Number (Mobile)");
-    
-   
-   // $fields->addFieldToTab("Root.Main", new CheckboxField('ShowOnHomePage'));
 
-    /*
-    $fields->addFieldToTab('Root.Main', new CalendarDateField('Date'), 'Content');
-    $fields->addFieldToTab('Root.Main', new TextField('Author'), 'Content');
-    */
-    return $fields;
-  }
-  
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Image', new UploadField('Photo'));
+
+        $fields->addFieldToTab('Root.JobDetail', new TextField('JobTitle', _t('Staff.JOB_TITLE', 'Job Title')));
+
+        $fields->addFieldToTab('Root.JobDetail', new EmailField('Email'));
+        $fields->addFieldToTab('Root.JobDetail', new TextField('TelephoneNumberDesk'));
+        $fields->addFieldToTab('Root.JobDetail', new TextField('TelephoneNumberMobile'));
+
+        $fields->renameField('TelephoneNumberDesk', 'Telephone Number (Landline)');
+        $fields->renameField('TelephoneNumberMobile', 'Telephone Number (Mobile)');
+
+        return $fields;
+    }
 }
 
-class Staff_Controller extends Page_Controller {
-  
+class Staff_Controller extends Page_Controller
+{
 }
-
-?>
